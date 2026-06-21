8f1aa0a967fa4789e7b876a2dd3d9abcd96fd6ae
Momoko Kaneko <golden.sps2014@gmail.com>
2026-06-21 10:44:45 +0900

    [add]ft_harvest_total.py

---
+++ ex3/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def    ft_harvest_total():
+       day_1_harvest = int(input("Day 1 harvest: "))
+       day_2_harvest = int(input("Day 2 harvest: "))
+       day_3_harvest = int(input("Day 3 harvest: "))
+       total = day_1_harvest + day_2_harvest + day_3_harvest
+       print(f"Total harvest: {total}") 
